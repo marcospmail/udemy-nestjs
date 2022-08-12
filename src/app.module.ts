97b0ee9a96381ce4804f68f70e80da82ac6f4702
Marcos Paulo <marcosp.mail@gmail.com>
2022-08-12 00:18:30 -0300

    Project setup

---
+++ src/app.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { MessagesModule } from './messages/messages.module';
+import { MessagesController } from './messages/messages.controller';
+
+@Module({
+  imports: [MessagesModule],
+  controllers: [AppController, MessagesController],
+  providers: [AppService],
+})
+export class AppModule {}
